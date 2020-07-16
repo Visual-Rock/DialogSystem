f4e5280aa851e34741deddfbbe8f41060a660d6c
Christoph <klon135christoph@gmail.com>
2020-07-16 17:39:04 +0200

    Update DialogGraphEditor.gd
    Commented Code

--- addons/DialogEditor/DialogGraphEditor/DialogGraphEditor.gd
+++ addons/DialogEditor/DialogGraphEditor/DialogGraphEditor.gd
@@ -7,8 +7,10 @@ var Graph : GraphEdit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#connects the id_pressed signal to add_node_menu_pressed
 	AddNodeMenu.get_popup().connect("id_pressed", self, "add_node_menu_pressed")
 	Graph = $VBoxContainer/GraphEdit
 
 func add_node_menu_pressed(id):
+	# calles the add_node function on the graph
 	Graph.add_node(id)
