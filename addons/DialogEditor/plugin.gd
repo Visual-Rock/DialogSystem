6874850ec5e7e5e8e19891db8a9c4cf6e4000cf9
Christoph <klon135christoph@gmail.com>
2020-07-16 15:44:29 +0200

    added scenes and edited scripts
    added the DialogGraphEdit (a simple GraphEdit Node)
    added DialogGraphEditor (for Editing Graphs)
    edited plugin.gd (added editortab for Dialog Editor)

--- addons/DialogEditor/plugin.gd
+++ addons/DialogEditor/plugin.gd
@@ -1,10 +1,40 @@
 tool
 extends EditorPlugin
 
+# DialogEditor Referencen 
+const DialogEditor         : Resource = preload("res://addons/DialogEditor/DialogGraphEditor/DialogGraphEditor.tscn")
+var   DialogEditorInstance : Node
 
-func _enter_tree():
-	pass
+# called when Plugin Enters Scene Tree
+func _enter_tree() -> void:
+	
+	# creates DialogEditorInstance
+	DialogEditorInstance = DialogEditor.instance()
+	
+	# gets the Editor Viewport and adds the DialogEditorInstance
+	get_editor_interface().get_editor_viewport().add_child(DialogEditorInstance)
+	# calls make_visible to make it invisible
+	make_visible(false)
 
+# called when Plugin exits the Scene Tree
+func _exit_tree() -> void:
+	
+	# checks if DialogEditorInstance is valid
+	if DialogEditorInstance:
+		# Queues the Editor for deletion
+		DialogEditorInstance.queue_free()
 
-func _exit_tree():
-	pass
+# makes the EditorInstance Visible or Invisible based on Input
+func make_visible(visible : bool) -> void:
+	# Checks if EditorInstance is valid
+	if DialogEditorInstance:
+		# sets visibility
+		DialogEditorInstance.visible = visible
+
+# Returns name for the Editor Tab
+func get_plugin_name() -> String:
+	return "Dialog Editor"
+
+# Returns the Icon for the Editor Tab
+func get_plugin_icon() -> Texture:
+	return get_editor_interface().get_base_control().get_icon("Node", "EditorIcons")
