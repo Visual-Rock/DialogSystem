104d808720b2b00b878bc2de6beca019bd2fb46f
Clon135 <klon135christoph@gmail.com>
2020-08-02 21:13:14 +0200

    Update BaseNode.gd
    - commented code

--- addons/DialogEditor/GraphEditor/Graph/Nodes/BaseNode.gd
+++ addons/DialogEditor/GraphEditor/Graph/Nodes/BaseNode.gd
@@ -2,7 +2,7 @@ tool
 extends GraphNode
 
 func _ready():
-	
+	# connects all signals
 	self.connect("close_request", self, "_on_BaseNode_close_request")
 	self.connect("resize_request", self, "_on_BaseNode_resize_request")
 
