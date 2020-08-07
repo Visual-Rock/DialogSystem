46bd5ea1df136ea893b48bd0642366901b40a4ec
Clon135 <klon135christoph@gmail.com>
2020-08-07 20:46:25 +0200

    Alpha finished
    - a basis
    - usefull for Linear Dialogs ( Branched Dialog will be added soon very soon )

--- Example/BasicExample.gd
+++ Example/BasicExample.gd
@@ -1,8 +1,26 @@
 extends Node2D
 
-func _ready():
-	var d : Dialog = Dialog.new()
-	var err = d.load_from_file("res://Dialogs/en/NPC_001.json", false)
-	print(err)
-	err = d.init_dialog()
-	print(err)
+var d : Dialog = Dialog.new()
+
+func _on_Button_pressed():
+	if d.inited_dialog == false:
+		print(d.load_from_file("res://Example/Bakes/en/Example_001.json"))
+		set_ui(d.get_values())
+		$DialogLayer/Control/VBoxContainer/HBoxContainer/Button.text = "next"
+	else:
+		if d.get_next_id() == 99:
+			set_ui({"Names": "Sys", "text": "End of Dialog this is not part of the dialog!"} )
+			$DialogLayer/Control/VBoxContainer/HBoxContainer/Button2.show()
+		else:
+			d.next()
+			set_ui(d.get_values())
+
+func set_ui(data : Dictionary) -> void:
+	$DialogLayer/Control/VBoxContainer/name.text = data["Names"]
+	$DialogLayer/Control/VBoxContainer/text.text = data["text"]
+
+
+func _on_Button2_pressed():
+	d.back_to_start()
+	set_ui(d.get_values())
+	$DialogLayer/Control/VBoxContainer/HBoxContainer/Button2.hide()
